d90522628d54ff130045bfee0d509c92501c69dd
fortress <fortress@realmz.cloudlab>
2015-02-12 07:39:23 -0700

    Can now specify an output file when decrypting, as well as an input text file.

--- undertone.py
+++ undertone.py
@@ -19,40 +19,48 @@
 # License along with this program.  If not, see
 # <http://www.gnu.org/licenses/>.
 
-import sys, os, random, hashlib, zlib
+import sys, os, random, zlib
 
 def main(argv):
 
-    if len(argv) == 4:
+    if len(argv) >= 4:
 
         action = argv[1]
         binary_chunk = argv[2]
         message = argv[3]
-        # Create ascii_chars and locations dictionaries from binary chunk:
-        key_dicts = process_chunk(binary_chunk)
 
         if str(argv[1]) == 'encrypt':
 
+            #   Use input message file in specified:
+            if message == '-f' and len(argv) == 5:
+                message_file = open(str(argv[4]),'rb')
+                message = message_file.read()
+
+            # Create ascii_chars and locations dictionaries from binary chunk:
+            key_dicts = process_chunk(binary_chunk)
             ascii_chars = key_dicts[0]
 
-            # Convert message to list of char locations:
+            #   Convert message to list of char locations:
             encrypted_msg = ""
 
             for char in message:
 
                 #   In the event of no char available in the ascii_chars dict,
-                #   fail silently by simply omitting the character from the
+                #   simply omit the char from the string, and print a warning
                 #   message.
                 if len(ascii_chars[char]) > 0:
 
                     random_char_loc = random.choice(ascii_chars[char])
                     encrypted_msg += str(random_char_loc) + '\n'
-                    # Remove used char loc. from list to avoid duplicate char loc. use
+
+                    #   Remove used char location from list to prevent the
+                    #   duplicate use of the char location, thus hampering
+                    #   frequency analysis.
                     ascii_chars[char].remove(random_char_loc)
 
                 else:
-                    print ('Not enough characters in pool: Omitting ' +
-                    'character.\nRemedy: Use a larger binary input file.\n')
+                    print ('WARNING: Not enough characters in pool: Omitting' +
+                    ' character.\nRemedy: Use a larger binary input file.\n')
 
             # Compress encrypted_msg with zlib and write to file crypt.msg:
             compressed_crypt = zlib.compress(encrypted_msg)
@@ -63,37 +71,42 @@ def main(argv):
             finally:
                 file.close()
 
-            print   # pretty terminal newline
-
         elif str(argv[1]) == 'decrypt':
 
-            #   To do: move the process_chunk() call so it only occurs if all
-            #   file exist etc... Currently it parses the whole file before
-            #   telling you that your input file is non-existent.
             decrypted_msg = ""
             decompressed_crypt = ""
             #   Using this zlib.decompressobj() allows silent decryption of
             #   corrupted or incomplete compressed data.
             decompressor = zlib.decompressobj()
             file = open(str(message),'rb')
+            # Create ascii_chars and locations dictionaries from binary chunk:
+            key_dicts = process_chunk(binary_chunk)
 
             try:
-                #   Note: It's important to use .read() rather than
-                #   .readline() ... The encrypted string sometimes gets
-                #   corrupted when being read with .readline(). (Not sure why)
                 decompressed_crypt = decompressor.decompress(file.read())
             finally:
                 file.close()
 
                 decrypted_msg = decrypt_msg(key_dicts,decompressed_crypt)
 
+            #   Write to output file if specified:
+            if len(argv) > 5 and argv[4] == '-o':
+
+                file = open(argv[5], 'wb')
+
+                try:
+                    file.write(decrypted_msg)
+                finally:
+                    file.close()
+            else:
                 print '\n' + decrypted_msg + '\n'
         else:
             print ('\nERROR: First argument must be either \'encrypt\' or ' +
                     '\'decrypt\'.\n')
     else:
-        print ('\nERROR: Too few command line arguments.\nTypical use: python ' +
-                'undertone.py encrypt/decrypt anyfile message.txt/crypt.msg\n')
+        print ('\nERROR: Too few command line arguments. Typical use:\n\n' +
+            'Encrypt: python undertone.py encrypt anyfile -f message\n' +
+        'Decrypt: python undertone.py decrypt anyfile crypt.msg -o output\n')
 
 def process_chunk(binary_chunk):
 
@@ -109,7 +122,8 @@ def process_chunk(binary_chunk):
                 location_list = [byte_location]
                 ascii_chars[str(byte)] = location_list
             else:
-                ascii_chars[str(byte)] += [byte_location]
+                #   Encoding to base36 slightly reduces encrypted msg filesize
+                ascii_chars[str(byte)] += [base36encode(byte_location)]
 
             locations_dict[byte_location] = str(byte)
             byte = chunk.read(1)
@@ -125,10 +139,34 @@ def decrypt_msg(key_dicts,crypt_msg):
 
     for location in crypt_msg.split('\n'):
         if location !=  '':
-            if int(location) in locations_dict: # Fail silently...
-                decrypted_msg += locations_dict[int(location)]
+            if base36decode(location) in locations_dict: # Fail silently...
+                decrypted_msg += locations_dict[int(base36decode(location))]
 
     return decrypted_msg
+
+def base36encode(number, alphabet='0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'):
+
+    if not isinstance(number, (int, long)):
+        raise TypeError('number must be an integer')
+
+    base36 = ''
+    sign = ''
+
+    if number < 0:
+        sign = '-'
+        number = -number
+
+    if 0 <= number < len(alphabet):
+        return sign + alphabet[number]
+
+    while number != 0:
+        number, i = divmod(number,len(alphabet))
+        base36 = alphabet[i] + base36
+
+    return sign + base36
+
+def base36decode(number):
+    return int(number,36)
 
 if __name__ == "__main__":
     main(sys.argv)
